2aced032cd7c6f0c3fe96af90bfef42bfbe9cc30
CitronsBlog <909229731@qq.com>
2020-03-29 00:41:12 +0800

    '初始化项目'

---
+++ src/components/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+
+  },
+  computer: {
+
+  },
+  created(){
+
+  }
+}
+</script>
+
+
+<style>
+
+</style>
