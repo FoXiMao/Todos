bb479b219b665d9278d0b909ac2f7c41888516da
CitronsBlog <909229731@qq.com>
2020-03-29 01:00:21 +0800

    '完成静态布局'

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
+  <div class="post">
+    <a-input placeholder="请输入任务" class="my_ipt"/>
+    <a-button type="primary" >添加事项</a-button>
 
+    <a-list bordered class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox>{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>1条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button >全部</a-button>
+          <a-button>未完成</a-button>
+          <a-button>已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a>清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
@@ -25,5 +49,7 @@ export default {
 
 
 <style>
-
+.post{
+  padding: 10px;
+}
 </style>
