4888dbb00260cdd19cd67b82bc996f06acc536f8
CitronsBlog <909229731@qq.com>
2020-03-29 00:54:41 +0800

    '安装所需依赖项'

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import stroe from './stroe'
+
+
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex)
+Vue.use(Antd)
+Vue.use(VueAxios, axios)
+
 
 /* eslint-disable no-new */
 new Vue({
